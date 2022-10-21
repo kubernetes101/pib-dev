b9c8c4f3e27ec11e94f2f2c4f2222770aff51495
Bart Robertson <bartr@microsoft.com>
2022-10-21 00:57:29 -0500

    Update post-setup.sh

--- vm/setup/post-setup.sh
+++ vm/setup/post-setup.sh
@@ -9,7 +9,7 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')  post start" >> "$HOME/status"
 docker pull ghcr.io/cse-labs/pib-webv:latest
 docker pull ghcr.io/cse-labs/pib-webv:beta
 
-if [ "$PIB_IS_INNER_LOOP" = "true" ]; then
+if [ "$PIB_IS_INNER_LOOP" != "true" ]; then
     kubectl run jumpbox --image=ghcr.io/cse-labs/jumpbox --restart=Always
 fi
 
