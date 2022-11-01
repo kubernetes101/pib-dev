64ad2e505431ff0ab066214a51ca9e7ddc4364bf
bartr <bartr@microsoft.com>
2022-11-01 03:31:39 +0000

    added

---
+++ apps/skumanager/src/CSE-Labs/Extensions/ErrorResult.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Net;
+
+namespace CseLabs.Middleware
+{
+    public class ErrorResult
+    {
+        public int Status => (int)Error;
+
+        public string Message { get; set; }
+
+        public HttpStatusCode Error { get; set; }
+    }
+}
