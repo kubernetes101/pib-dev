80fee1e369781b7b03a7a5c401866849ed0fa7c5
bartr <bartr@microsoft.com>
2022-11-03 15:52:04 -0500

    updated swagger

--- apps/coffeeshop/src/Controllers/DrinksController.cs
+++ apps/coffeeshop/src/Controllers/DrinksController.cs
@@ -38,5 +38,78 @@ namespace CoffeeShop.Controllers
 
             return Ok(db.Drinks.Values);
         }
+
+        /// <summary>
+        /// Get all Coffees
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        [HttpGet(Name = "GetCoffee")]
+        [Route("api/v1/[controller]/coffee")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Drink>))]
+        [SwaggerOperation(Summary = "Get all Coffees", Description = "Get all Coffees")]
+        public IActionResult GetCoffees()
+        {
+            Logger.LogInformation("get", "GetCoffee");
+            return Ok(GetDrinksByCategory("Coffee"));
+        }
+
+        /// <summary>
+        /// Get all Teas
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        [HttpGet(Name = "GetTea")]
+        [Route("api/v1/[controller]/tea")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Drink>))]
+        [SwaggerOperation(Summary = "Get all Teas", Description = "Get all Teas")]
+        public IActionResult GetTea()
+        {
+            Logger.LogInformation("get", "GetTea");
+            return Ok(GetDrinksByCategory("Tea"));
+        }
+
+        /// <summary>
+        /// Get all Lemonade
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        [HttpGet(Name = "GetCoffee")]
+        [Route("api/v1/[controller]/lemonade")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Drink>))]
+        [SwaggerOperation(Summary = "Get all Lemonade", Description = "Get all Lemonade")]
+        public IActionResult GetLemonade()
+        {
+            Logger.LogInformation("get", "GetLemonade");
+            return Ok(GetDrinksByCategory("Lemonade"));
+        }
+
+        /// <summary>
+        /// Get all Water
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        [HttpGet(Name = "GetWater")]
+        [Route("api/v1/[controller]/water")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Drink>))]
+        [SwaggerOperation(Summary = "Get all Water", Description = "Get all Water")]
+        public IActionResult GetWater()
+        {
+            Logger.LogInformation("get", "GetWater");
+            return Ok(GetDrinksByCategory("Water"));
+        }
+
+        private static List<Drink> GetDrinksByCategory(string category)
+        {
+            Database db = new();
+
+            List<Drink> list = new();
+
+            foreach (var d in db.Drinks.Values)
+            {
+                if (d.Category == category)
+                {
+                    list.Add(d);
+                }
+            }
+
+            return list;
+        }
     }
 }
