aa9205905f20d53ad35052512c30131330a235f1
bartr <bartr@microsoft.com>
2022-11-30 04:20:07 +0000

    added

---
+++ apps/myapp/src/CSE-Labs/Logger/CseLoggerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace CseLabs.Middleware
+{
+    /// <summary>
+    /// Logger Configuration
+    /// </summary>
+    public class CseLoggerConfiguration
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+    }
+}
