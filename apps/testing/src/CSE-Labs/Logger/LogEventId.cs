eb52396cb5277fc1ec035ecc22c47ec2650fb256
aflinchb <anflinch@microsoft.com>
2023-02-03 21:46:26 +0000

    added

---
+++ apps/testing/src/CSE-Labs/Logger/LogEventId.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace CseLabs.Middleware
+{
+    /// <summary>
+    /// Nullable EventId class for logging
+    /// </summary>
+    public class LogEventId
+    {
+        public LogEventId(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
